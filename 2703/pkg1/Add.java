67cf6f9655590183fb3021b3b1db80778cc938ce
Hari2020codes <160399623+Hari2020codes@users.noreply.github.com>
2026-03-27 16:20:28 +0530

    Add files via upload

---
+++ 2703/pkg1/Add.java
@@ -0,0 +1,9 @@
+package pkg1;
+public class Add implements Calculate
+{
+public void cal(int x,int y)
+{
+   
+    System.out.println("Sum of Nos="+(x+y));
+}
+}
---
+++ 2703/pkg1/Calculate.java
@@ -0,0 +1,5 @@
+package pkg1;
+public interface Calculate
+{
+    void cal(int x,int y);
+}
