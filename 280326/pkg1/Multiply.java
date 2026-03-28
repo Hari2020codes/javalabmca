c3b8ad985c271ebf6c7c542d5ddef47a3a362b74
Hari2020codes <160399623+Hari2020codes@users.noreply.github.com>
2026-03-28 12:08:06 +0530

    Add files via upload

---
+++ 280326/pkg1/Multiply.java
@@ -0,0 +1,8 @@
+package pkg1;
+public class Multiply implements Calculate
+{
+public void cal(int x,int y)
+{
+    System.out.println("Multiplication="+(x*y));
+}
+}
---
+++ 280326/pkg1/Subtract.java
@@ -0,0 +1,9 @@
+package pkg1;
+public class Subtract implements Calculate
+{
+public void cal(int x,int y)
+{
+
+    System.out.println("Difference="+(x-y));
+}
+}
